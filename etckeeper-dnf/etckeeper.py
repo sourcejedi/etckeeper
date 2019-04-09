5fc51327943c31e0d1d02e9724ea30c545b37023
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2019-04-10 00:19:14 +0100

    DNF: fix logging, now it will work from Ansible [OLD]
    [OLD] One bug has been removed from this commit.
    I think one at least one deficit remains, regarding how
    clearly the exit status is logged, or how we raise
    exceptions (instead of catching them all, to avoid
    breaking dnf).
    
    The Ansible dnf module uses the python dnf bindings.  In contexts like
    these, stdout/stderr is owned by the host app (Ansible).  dnf should not
    mess with stdout/stderr, unless the host app asks it to log there.
    
    Specifically, it was breaking the JSON output of the Ansible module.
    This was only noticeable when the etckeeper message began with a "["
    character.  Ansible has a mechanism to try and skip header messages like
    login banners.  However, "[" is a valid character to start a JSON document.
    
    https://unix.stackexchange.com/questions/511210/ansible-dnf-module-module-failure/
    
    This requires decoding the etckeeper messages into unicode.  For gory
    details, see the code comment.
    
    Also enable unicode string literals.  This reduces differences between py2
    and py3 semantics; it is universal inside the dnf code.

--- etckeeper-dnf/etckeeper.py
+++ etckeeper-dnf/etckeeper.py
@@ -7,9 +7,11 @@
 # Distutils code below was copied from etckeeper-bzr distributed with v1.15
 #
 
+from __future__ import unicode_literals
 from dnfpluginscore import logger
 
-import os
+import subprocess
+import locale
 import dnf
 
 
@@ -17,23 +19,70 @@ class Etckeeper(dnf.Plugin):
 
     name = 'etckeeper'
 
-    def _out(self, msg):
+    def _debug(self, msg):
         logger.debug('Etckeeper plugin: %s', msg)
 
+    def _log_pipe(self, pipe):
+        # etckeeper & git messages should be encoded using the default locale
+        # (or us-ascii, which is a strict subset).
+        #
+        # Normally py2 breaks if you print arbitrary unicode when stdout is
+        # not a tty (UnicodeEncodeError).  However the dnf cli has a
+        # workaround; it will survive regardless of what we do.
+        #
+        # Then we have logging.FileHandler.  In py3 it will use
+        # locale.getpreferredencoding(False) by default.  This should match
+        # the default locale, unless we are on py < 3.2, AND the program
+        # forgot to call setlocale(LC_ALL, "").  dnf already calls
+        # setlocale(LC_ALL, ""), so it will be nice and consistent.
+        # In fact it is the dnf *library* that calls setlocale, this is not
+        # really recommended, but it makes me pretty confident here.
+        #
+        # errors='replace' means that decode errors give us '\ufffd', which
+        # causes UnicodeEncodeError in some character encodings.  Let us
+        # simulate a round-trip through errors='replace', by replacing them
+        # with a question mark.
+        #
+        # The story for py2 is more complex.  In libdnf 2.6.3, the logfile
+        # is equivalent to hardcoding a utf8 encoding.  That is survivable
+        # (and if it changes to match py3, it will also be fine).
+        #
+        encoding = locale.getpreferredencoding(False)
+        for line in pipe:
+            line = line.decode(encoding, 'replace')
+            line.replace('\ufffd', '?')
+            line = line.rstrip('\n')
+            logger.info('%s', line)
+
     def resolved(self):
-        self._out('pre transaction commit')
-        command = '%s %s' % ('etckeeper', " pre-install")
-        ret = os.system(command)
+        self._debug('pre transaction commit')
+        proc = subprocess.Popen(("etckeeper", "pre-install"),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                close_fds=True)
+        self._log_pipe(proc.stdout)
+        ret = proc.wait()
         if ret != 0:
-            raise dnf.exceptions.Error('etckeeper returned %d' % (ret >> 8))
+            raise dnf.exceptions.Error('etckeeper returned %d' % ret)
 
     def transaction(self):
-        self._out('post transaction commit')
-        command = '%s %s > /dev/null' % ('etckeeper', "post-install")
-        os.system(command)
+        self._debug('post transaction commit')
+        with open("/dev/null", "wb") as devnull:
+            proc = subprocess.Popen(("etckeeper", "post-install"),
+                                    stdout=devnull,
+                                    stderr=subprocess.PIPE,
+                                    close_fds=True)
+        self._log_pipe(proc.stderr)
+        ret = proc.wait()
+        if ret != 0:
+            logger.err('etckeeper returned %d' % ret)
 
 if __name__ == "__main__":
     from distutils.core import setup
-    setup(name="dnf-etckeeper",
-          packages=["dnf-plugins"],
-          package_dir={"dnf-plugins":"etckeeper-dnf"})
+
+    # Hack: distutils in py2 does not like unicode literals.
+    # Convert them to strings.
+    # In py3, this does not make a difference.
+    setup(name=str("dnf-etckeeper"),
+          packages=str(["dnf-plugins"]),
+          package_dir={str("dnf-plugins"):str("etckeeper-dnf")})
